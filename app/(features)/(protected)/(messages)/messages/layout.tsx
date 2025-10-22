14f156a2116a33198ad4dc462171dc0ab98b066f
Usef <yosefarafa198@gmail.com>
2025-10-22 16:58:48 +0300

    refactor: Remove Header From /messages it's avillable in global layout.

--- app/(features)/(protected)/(messages)/messages/layout.tsx
+++ app/(features)/(protected)/(messages)/messages/layout.tsx
@@ -1,12 +1,9 @@
-import { decodedUser } from "@/app/(shared)/helpers";
 import React from "react";
-import { Header } from "../../(shared)/_components";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
   //   const mode: "client" | "freelancer" = (await decodedUser()).role!;
   return (
     <>
-      <Header userType="client" />
       {children}
     </>
   );
--- app/(features)/(protected)/(messages)/messages/page.tsx
+++ app/(features)/(protected)/(messages)/messages/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { Suspense } from "react";
 const MessagePageContent = dynamic(
   () => import("../_components/MessagePageContent")
